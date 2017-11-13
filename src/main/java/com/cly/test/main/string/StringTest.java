53e9a9997c3368104580bc95313de0d2f860c89d
zheng jiang <jiang.zheng@msxf.com>
2017-11-13 11:19:18 +0800

    添加string的split测试

--- src/main/java/com/cly/test/main/string/StringTest.java
+++ src/main/java/com/cly/test/main/string/StringTest.java
@@ -1,5 +1,7 @@
 package com.cly.test.main.string;
 
+import org.springframework.util.StringUtils;
+
 /**
  * @ Filename FastJsonControllerTest.java
  * @ Description
@@ -12,10 +14,19 @@ package com.cly.test.main.string;
 public class StringTest {
 
     public static void main(String[] args){
-
+        String str = "123,4567,,89,,,,";
+        for(String split: splitComma(str)){
+            System.out.println(split);
+        }
     }
 
-    //
-
+    //split分隔末尾多个逗号，保留末尾空字符
+    private static String[] splitComma(String str){
+        return str.split(",",-1);
+    }
+    //split分隔末尾多个逗号,不保留末尾空字符
+    private static String[] splitComma2(String str){
+        return str.split(",",-1);
+    }
 
 }
